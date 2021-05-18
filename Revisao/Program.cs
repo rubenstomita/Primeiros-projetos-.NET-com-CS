b99998e06340d4bebf9bf65cb2dded27b154d930
tomita <rubenstomita@yahoo.com.br>
2021-05-18 00:32:04 -0300

    primeiro commit

---
+++ Revisao/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada:");
+            Consele.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //ToDo: adicionar aluno
+                        break;
+                    case "2":
+                        //ToDo: listar alunos
+                        break;
+                    case "3":
+                        //ToDo: calcular média geral
+                        break;                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                Console.WriteLine("Informe a opção desejada:");
+                Consele.WriteLine("1- Inserir novo aluno");
+                Console.WriteLine("2- Listar alunos");
+                Console.WriteLine("3- Calcular média geral");
+                Console.WriteLine("X- Sair");
+                Console.WriteLine();
+
+                opcaoUsuario = Console.ReadLine();  
+            }
+        }
+    }
+}
